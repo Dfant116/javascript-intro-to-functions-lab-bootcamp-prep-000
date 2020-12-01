07aeeb07a283f761faa395af1dc0ef0ea8a22b0f
Dalton Fant <daltonfant16@gmail.com>
2020-12-01 03:29:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function shout(string) {
 function whisper(string){
   return string.toLowerCase()
 }
-function sayHiToGrandma(string){
-  if string.lowercase
-    return ("I can't hear you!")
+function logShout(string){
+  console.log(string.toUpperCase())
 }
+
